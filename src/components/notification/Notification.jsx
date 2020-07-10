6e64af3750300b982f58c33c0c1d39cb366943db
Georges <georges.guelton@gmail.com>
2020-07-10 15:53:54 +0200

    fix font size notification box

--- src/components/notification/Notification.jsx
+++ src/components/notification/Notification.jsx
@@ -12,7 +12,7 @@ const Notification = () => {
                                 <i className="fas fa-bullhorn icon-box-1"></i>
                             </div>
                             <div className='mx-5'>
-                                <p className='text-white m-0'>You have a new notification waiting for you !</p>
+                                <p className='text-white m-0 notification-text-size'>You have a new notification waiting for you !</p>
                             </div>
                         </div>
 
@@ -21,7 +21,7 @@ const Notification = () => {
                                 <i className="fas fa-exclamation-triangle icon-box-2"></i>
                             </div>
                             <div className='margin-left-notification-2'>
-                                <p className='text-notification-2 m-0'>You have one task left ! Go on !</p>
+                                <p className='text-notification-2 m-0 notification-text-size'>You have one task left ! Go on !</p>
                             </div>
                         </div>
 
@@ -31,7 +31,7 @@ const Notification = () => {
                                 <i className="fas fa-check-circle icon-box-3"></i>
                             </div>
                             <div className='margin-left-notification-3'>
-                                <p className='text-notification-3 m-0'>You finished your project ! GGWP !</p>
+                                <p className='text-notification-3 m- notification-text-size'>You finished your project ! GGWP !</p>
                             </div>
                         </div>
                     </div>
